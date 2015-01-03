f54190f4374e142916fce306ca3d919c052fa647
Jim Bark <jimbark007@gmail.com>
2015-01-03 09:25:26 +0000

    first commit

---
+++ README.md
@@ -0,0 +1,13 @@
+setup.git
+=========
+Clone and run this on a new CentOS thub.com:jimbark/testproject.git7 VM instance to
+configure both the machine and development environment as
+follows:
+
+```sh
+cd $HOME
+sudo yum install -y git
+git clone https://github.com:jimbark/c7setup.git
+./setup/setup.sh   
+```
+
---
+++ setup.sh
@@ -0,0 +1,48 @@
+#!/bin/bash
+# Simple setup.sh for configuring CentOS 7 instance
+# for headless setup. 
+
+cd $HOME
+
+# update yum repo info 
+sudo yum update -y
+
+# install git
+sudo yum install -y git
+
+# Load node and npm from the EPEL repository
+sudo yum install -y epel-release
+sudo yum install -y nodejs npm --enablerepo=epel
+
+# Install jshint to allow checking of JS code within emacs
+# http://jshint.com/
+npm install -g jshint
+
+# Install rlwrap to provide libreadline features with node
+# See: http://nodejs.org/api/repl.html#repl_repl
+sudo yum install -y rlwrap
+
+# Install emacs24
+# https://launchpad.net/~cassou/+archive/emacs
+sudo yum install -y emacs
+
+# Install Heroku toolbelt, and ruby which it needs
+# https://toolbelt.heroku.com/debian
+sudo yum install -y ruby
+wget -qO- https://toolbelt.heroku.com/install.sh | sh
+
+# git pull and install dotfiles as well
+cd $HOME
+if [ -d ./dotfiles/ ]; then
+    mv dotfiles dotfiles.old
+fi
+if [ -d .emacs.d/ ]; then
+    mv .emacs.d .emacs.d~
+fi
+git clone https://github.com:jimbark/c7dotfiles.git
+ln -sb dotfiles/.screenrc .
+ln -sb dotfiles/.bash_profile .
+ln -sb dotfiles/.bashrc .
+ln -sb dotfiles/.bashrc_custom .
+ln -sf dotfiles/.emacs.d .
+
